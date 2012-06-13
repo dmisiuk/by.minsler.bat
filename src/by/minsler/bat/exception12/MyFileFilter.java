d80d58fcc4595c4ab8eb5832f155d67709fae7dc
Dzmitry Misiuk <minsler.by@gmail.com>
2012-06-13 19:42:14 +0300

    Optimized MyFileFilter(String have method endWith())

--- src/by/minsler/bat/exception12/MyFileFilter.java
+++ src/by/minsler/bat/exception12/MyFileFilter.java
@@ -14,21 +14,26 @@ public class MyFileFilter implements FileFilter {
 	@Override
 	public boolean accept(File f) {
 		String fileName = f.getName();
-		int i = fileName.length();
-		StringBuilder ext = new StringBuilder();
-		do {
-			i--;
-			ext.insert(0, fileName.charAt(i));
-		} while (i > 0 && fileName.charAt(i) != '.');
+		// int i = fileName.length();
+		// StringBuilder ext = new StringBuilder();
+		// do {
+		// i--;
+		// ext.insert(0, fileName.charAt(i));
+		// } while (i > 0 && fileName.charAt(i) != '.');
 
 		// while (i > -1 && fileName.charAt(i) != '.') {
 		// ext.insert(0, fileName.charAt(i));
 		// i--;
 		// }
 
-		if (ext.toString().equalsIgnoreCase(this.extension) && f.isFile()) {
+		// if (ext.toString().equalsIgnoreCase(this.extension) && f.isFile()) {
+		// return true;
+		// }
+
+		if (fileName.endsWith(extension) && f.isFile()) {
 			return true;
 		}
 		return false;
+
 	}
 }
