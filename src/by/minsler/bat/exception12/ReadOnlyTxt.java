453a960f7e16f47b9a4b372429dd5608d45fb49d
Dzmitry Misiuk <minsler.by@gmail.com>
2012-06-13 21:07:51 +0300

    Tested MyFileFilter

--- src/by/minsler/bat/exception12/ReadOnlyTxt.java
+++ src/by/minsler/bat/exception12/ReadOnlyTxt.java
@@ -7,18 +7,15 @@ public class ReadOnlyTxt {
 	public static void main(String[] args) {
 		File dir = new File("/Users/minsler/test");
 
-		try {
-			if (dir.exists() && dir.isDirectory()) {
-				File[] files = dir.listFiles(new MyFileFilter("dima"));
+		if (dir.exists() && dir.isDirectory()) {
+			File[] files = dir.listFiles(new MyFileFilter("txt"));
 
-				for (int i = 0; i < files.length; i++) {
-					System.out.println(files[i].getName());
-				}
-			} else {
-				System.out.println("Direcotry not exist or is nod directory");
+			for (int i = 0; i < files.length; i++) {
+				System.out.println(files[i].getName());
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
+		} else {
+			System.out.println("Direcotry not exist or is not directory");
 		}
+
 	}
 }
